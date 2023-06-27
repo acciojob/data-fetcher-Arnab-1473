bbe0d5c8e851198f0eb885c3594a4a2e9025ba60
Arnab-1473 <arban.nilr.1473@gmail.com>
2023-06-27 09:54:29 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
-
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = () => {
+      fetch('https://dummyjson.com/products')
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+      })
+      .catch(error => console.log("Error", error))
+    }
+    fetchData();
+  }, []);
+    
   return (
     <div>
-        {/* Do not remove the main div */}
+      {data.length > 0 ? (
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>{item.products.brand}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading data...</p>
+      )}
     </div>
   )
 }
 
-export default App
+export default App;
